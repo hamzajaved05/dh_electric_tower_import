158614b97b5e9e17735e1e0871fc8e438f595394
hamzawlptp <hamzajaveddh@bitbucket.com>
2023-06-19 15:26:50 +0200

    small mods

--- script_for_adding_names.py
+++ script_for_adding_names.py
@@ -1,11 +1,14 @@
+import warnings
 import xml.etree.ElementTree as ET
 from nmk.GeoUtils import CHtoWGS84
 import numpy as np
 from pyproj import CRS, Transformer
 from nmk.GeoUtils import GeoToCartesianMeters
 from tower.pointofinterest import POI
+import json
 
-def convert_coordinates(x, y, height=None):
+
+def convert_coordinates(x: float, y: float, height: float = None):
     # Define the Coordinate Reference Systems
     src_crs = CRS("EPSG:2056")  # CH1903+ / LV95
     dest_crs = CRS("EPSG:4326")  # WGS84
@@ -17,7 +20,8 @@ def convert_coordinates(x, y, height=None):
     lon, lat = transformer.transform(x, y)
     return [lon, lat] if height is None else [lon, lat, height]
 
-def build_dict(xml_file):
+
+def build_dict(xml_file: str):
     # Initialize the dictionary
     structure_dict = {}
 
@@ -38,34 +42,33 @@ def build_dict(xml_file):
 
                 if struct_number_elem is not None and x_elem is not None and y_elem is not None and z_elem is not None:
                     # Add to dictionary as a tuple (value, units)
-                    structure_dict[struct_number_elem.text] = {"coordinates": 
+                    structure_dict[struct_number_elem.text] = {"coordinates":
                         convert_coordinates(
-                        float(x_elem.text),
-                        float( y_elem.text), 
-                        float(z_elem.text))
-                       }
+                            float(x_elem.text),
+                            float(y_elem.text),
+                            float(z_elem.text))
+                    }
                 structure_dict[struct_number_elem.text]["insulator_attach_points"] = []
                 structure_dict[struct_number_elem.text]["wire_attach_points"] = []
                 structure_dict[struct_number_elem.text]["name"] = []
-    
-    
+
     for table in root.iter('table'):
         if table.attrib.get('plsname', '') == 'Structure Attachment Coordinates':
             for struct_report in table.iter('structure_attachment_coordinates'):
                 struct_number_elem = struct_report.find('struct_number')
                 structure_dict[struct_number_elem.text]["insulator_attach_points"].append(
                     convert_coordinates(
-                    float(struct_report.find('insulator_attach_point_x').text),
-                    float(struct_report.find('insulator_attach_point_y').text),
-                    float(struct_report.find('insulator_attach_point_z').text),
-                ))
+                        float(struct_report.find('insulator_attach_point_x').text),
+                        float(struct_report.find('insulator_attach_point_y').text),
+                        float(struct_report.find('insulator_attach_point_z').text),
+                    ))
 
                 structure_dict[struct_number_elem.text]["wire_attach_points"].append(
                     convert_coordinates(
-                    float(struct_report.find('wire_attach_point_x').text),
-                    float(struct_report.find('wire_attach_point_y').text),
-                    float(struct_report.find('wire_attach_point_z').text),
-                ))
+                        float(struct_report.find('wire_attach_point_x').text),
+                        float(struct_report.find('wire_attach_point_y').text),
+                        float(struct_report.find('wire_attach_point_z').text),
+                    ))
 
                 structure_dict[struct_number_elem.text]["name"].append(struct_report.find('set_label').text)
 
@@ -78,10 +81,7 @@ def build_dict(xml_file):
 
 
 # Usage:
-xml_file = '/mnt/00A03D3C0BCCF8D8/Data/PowerLinesData/20230508SwissGrid/testLine/TR1670_Riddes-Avise_Valpeline_IT/TR1670_Riddes-Avise_Valpeline_IT.xml'
-xml_dict = build_dict(xml_file)
 
-import json
 
 def get_geojson(file_path):
     # Read GeoJSON file
@@ -89,35 +89,47 @@ def get_geojson(file_path):
         data = json.load(f)
     return data
 
-def writ_geojson(file_path, geojson):
+
+def write_geojson(file_path, geojson):
     # Write GeoJSON file
     with open(file_path, 'w') as f:
-        json.dump(geojson, f, indent = "\t")
+        json.dump(geojson, f, indent="\t")
 
-file_path = '/mnt/00A03D3C0BCCF8D8/Data/PowerLinesData/20230508SwissGrid/testLine/TR1670_Riddes-Avise_Valpeline_IT/TR1670_Riddes-Avise_Valpeline_IT_raw.json'
-json_dict = get_geojson(file_path)
 
-def getGeoCoordinatesPoi(feature, index):
-    poi =  feature["properties"]["pois"][index]
+def get_geo_coordinates_poi(feature, index):
+    poi = feature["properties"]["pois"][index]
 
-def add_names(json_dict, xml_dict):
+
+def add_names(json_dict: dict, xml_dict: dict) -> dict:
     # vectorizedFn = np.vectorize(GeoToCartesianMeters)
+
+    towers_in_json: int = sum(
+        [1 for feature in json_dict["features"] if feature["properties"]["type"] == "electric_pole"])
+    towers_in_xml: int = len(xml_dict)
+
+    # solve this issue! get from gil and check
+    if towers_in_json != towers_in_xml:
+        warnings.warn(f"Incorrect matching for number of towers -> xml: {towers_in_xml} vs json -> {towers_in_xml}")
+
     xml_names = [key for key in xml_dict.keys()]
     xml_coordinates = np.array([xml_dict[key]["coordinates"][:2] for key in xml_dict.keys()])
 
+    tower_association_deltas = []
+    skipped_towers = 0
     for feature in json_dict["features"]:
         if feature["properties"]["type"] == "electric_pole":
 
             # add tower name
             json_coords = feature["geometry"]["coordinates"][::-1]
-            distances = np.linalg.norm([GeoToCartesianMeters(json_coords, xml_coord) for xml_coord in xml_coordinates], axis=1)
+            distances = np.linalg.norm([GeoToCartesianMeters(json_coords, xml_coord) for xml_coord in xml_coordinates],
+                                       axis=1)
             min_index = np.argmin(distances)
 
             if distances[min_index] < 1e-3:
                 feature["properties"]["name"] = xml_names[min_index]
                 print("Tower found for coordinates: {}: {}".format(feature["properties"]["name"], distances[min_index]))
 
-
+                tower_association_deltas.append(distances[min_index])
                 # add poi names
                 # pois_xml = xml_dict[xml_names[min_index]]["insulator_attach_points"]
                 # print(f"Compare_pois -> {len(pois_xml)} vs {len(feature['properties']['pois']) - 1}")
@@ -130,7 +142,7 @@ def add_names(json_dict, xml_dict):
                 #     coords = POI(poi["height"], poi["distance"], poi["azimuth"]).get_lat_lon(towerBaseLatLon, asl = asl)
                 #     distances = np.linalg.norm([GeoToCartesianMeters(coords, xml_coord) for xml_coord in pois_xml], axis=1)
                 #     min_index_poi = np.argmin(distances)
-                    
+
                 #     if distances[min_index_poi] > 1.5:
                 #         continue
 
@@ -144,9 +156,21 @@ def add_names(json_dict, xml_dict):
                 xml_names.pop(min_index)
             else:
                 print("No tower found for coordinates: {}: {}".format(feature["properties"]["name"], json_coords))
-
+                skipped_towers += 1
+    print(f"Missing towers {xml_names}")
+    print(
+        f"Matching towers {len(tower_association_deltas)}\nmax delta {np.max(tower_association_deltas)}\nskipped towers from json {skipped_towers}\nxml towers {towers_in_xml}\njson towers {towers_in_json}")
     return json_dict
 
-json_dict2 = add_names(json_dict, xml_dict)
-writ_geojson('/mnt/00A03D3C0BCCF8D8/Data/PowerLinesData/20230508SwissGrid/testLine/TR1670_Riddes-Avise_Valpeline_IT/TR1670_Riddes-Avise_Valpeline_IT.json', json_dict2)
-print(json_dict)
+
+if __name__ == "__main__":
+    file_path = '/mnt/00A03D3C0BCCF8D8/Data/PowerLinesData/20230508SwissGrid/testLine/TR1670_Riddes-Avise_Valpeline_IT/TR1670_Riddes-Avise_Valpeline_IT.json'
+    json_dict: dict = get_geojson(file_path)
+    xml_file = '/mnt/00A03D3C0BCCF8D8/Data/PowerLinesData/20230508SwissGrid/testLine/TR1670_Riddes-Avise_Valpeline_IT/TR1670_Riddes-Avise_Valpeline_IT.xml'
+    xml_dict: dict = build_dict(xml_file)
+
+    json_dict2: dict = add_names(json_dict, xml_dict)
+    write_geojson(
+        '/mnt/00A03D3C0BCCF8D8/Data/PowerLinesData/20230508SwissGrid/testLine/TR1670_Riddes-Avise_Valpeline_IT/TR1670_Riddes-Avise_Valpeline_IT_processed.json',
+        json_dict2)
+    # print(json_dict)
